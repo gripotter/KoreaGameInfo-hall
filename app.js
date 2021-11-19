bfb3fa14ea5c5ab171df13db745933e32c0cb577
gripotter <87414790+gripotter@users.noreply.github.com>
2021-11-20 00:18:20 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const idx3=document.querySelector("#idx3");
 
 //idx1 호버링
 function HandleMouseEnterIdx1(){
-    idx1.style.fontSize="7.5vw";
+    idx1.style.fontSize="4.5vw";
     idx1.style.color="rgb(214, 28, 75)";
 }
 function HandleMouseLeaveIdx1(){
-    idx1.style.fontSize="7vw";
+    idx1.style.fontSize="4vw";
     idx1.style.color="white";
 }
 idx1.addEventListener("mouseenter",HandleMouseEnterIdx1);
@@ -17,11 +17,11 @@ idx1.addEventListener("mouseleave",HandleMouseLeaveIdx1);
 
 //idx2 호버링
 function HandleMouseEnterIdx2(){
-    idx2.style.fontSize="7.5vw";
+    idx2.style.fontSize="4.5vw";
     idx2.style.color="rgb(214, 28, 75)";
 }
 function HandleMouseLeaveIdx2(){
-    idx2.style.fontSize="7vw";
+    idx2.style.fontSize="4vw";
     idx2.style.color="white";
 }
 idx2.addEventListener("mouseenter",HandleMouseEnterIdx2);
@@ -29,11 +29,11 @@ idx2.addEventListener("mouseleave",HandleMouseLeaveIdx2);
 
 //idx3 호버링
 function HandleMouseEnterIdx3(){
-    idx3.style.fontSize="7.5vw";
+    idx3.style.fontSize="4.5vw";
     idx3.style.color="rgb(214, 28, 75)";
 }
 function HandleMouseLeaveIdx3(){
-    idx3.style.fontSize="7vw";
+    idx3.style.fontSize="4vw";
     idx3.style.color="white";
 }
 idx3.addEventListener("mouseenter",HandleMouseEnterIdx3);
